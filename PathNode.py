660e408aeffbda8d1fc2510e444742b5c0470eef
amir <amirsabbagh2000z@gmail.com>
2021-03-27 12:10:50 +0430

    add new attributes to PathNode

--- PathNode.py
+++ PathNode.py
@@ -1,15 +1,29 @@
 class PatchNode:
-    def __init__(self, whichButter, whichPerson, pathString, cost):
+    def __init__(self, whichButter, whichPerson, pathString, cost,unvisitedButters,unvisitedPersons):
         self.__whichButter = whichButter
         self.__whichPerson = whichPerson
         self.__pathString = pathString
         self.__cost = cost
+        self.__unvisitedButters = unvisitedButters
+        self.__unvisitedPersons = unvisitedPersons
 
     def getWhichButter(self):
         return self.__whichButter
 
     def setWhichButter(self, whichButter):
         self.__whichButter = whichButter
+
+    def getUnvisitedButters(self):
+        return self.__unvisitedButters
+
+    def setUnvisitedButters(self, unvisitedButters):
+        self.__unvisitedButters = unvisitedButters
+
+    def getUnvisitedPersons(self):
+        return self.__unvisitedButters
+
+    def setUnvisitedPersons(self, unvisitedPersons):
+        self.unvisitedPersons = unvisitedPersons
 
     def getWhichPerson(self):
         return self.__whichPerson
