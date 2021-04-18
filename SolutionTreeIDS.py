0d6188e3dcf267b5223dc21fbf07fef1571bf74a
Faraz <f.farangizadeh@gmail.com>
2021-04-18 08:46:12 +0430

    Debug multiThread

--- SolutionTreeIDS.py
+++ SolutionTreeIDS.py
@@ -1,9 +1,42 @@
+import _thread
 from copy import copy, deepcopy
+
 from GraphOperationsIDS import GraphOperations
 from PathNode import PathNode
 from State import State
 
-import _thread
+
+def updateDataAfterSimpleIDS(new_node, table, graph, parent):
+    table[parent.getState().getRobot().getLocation()[0]][
+        parent.getState().getRobot().getLocation()[1]].setHaveRobot(False)
+    table[new_node.getState().getRobot().getLocation()[0]][
+        new_node.getState().getRobot().getLocation()[1]].setHaveRobot(True)
+    graph.blocks = table
+    graph.robot.setLocation((new_node.getState().getRobot().getLocation()[0],
+                             new_node.getState().getRobot().getLocation()[1]))
+
+
+def calculateEmptyAroundOfButter(butter, table):
+    ans = []
+    length = butter.getLocation()[1]
+    height = butter.getLocation()[0]
+    if table[height][length - 1].getHaveButter() or table[height][length - 1].getHaveObstacle():
+        pass
+    else:
+        ans.append(1)
+    if table[height - 1][length].getHaveButter() or table[height - 1][length].getHaveObstacle():
+        pass
+    else:
+        ans.append(2)
+    if table[height][length + 1].getHaveButter() or table[height][length + 1].getHaveObstacle():
+        pass
+    else:
+        ans.append(3)
+    if table[height + 1][length].getHaveButter() or table[height + 1][length].getHaveObstacle():
+        pass
+    else:
+        ans.append(4)
+    return ans
 
 
 class SolutionTreeIDS:
@@ -18,22 +51,19 @@ class SolutionTreeIDS:
 
     def start(self):
         finalList = []
+        temp = []
         currentNode = self.__startingNodes.pop(0)
         for b in currentNode.getUnvisitedButters():
             for p in currentNode.getUnvisitedPersons():
-                for side in self.calculateEmptyAroundOfButter(b, currentNode.table):
-                    print(b.getNum(), " ", p.getNum(), " ", side)
-                    # print(b.getLocation())
+                for side in calculateEmptyAroundOfButter(b, currentNode.table):
 
                     graph = GraphOperations(currentNode.table, self.__butters,
                                             deepcopy(currentNode.getState().getRobot()), self.__persons)
-                    print("Start")
                     tup1 = graph.IDS(currentNode.getState(), b, side)
-                    print("end")
                     if tup1 is None:
                         continue
                     new_node = tup1[0]
-                    self.updateDataAfterSimpleIDS(new_node, currentNode.table, graph , currentNode)
+                    updateDataAfterSimpleIDS(new_node, currentNode.table, graph, currentNode)
 
                     tup2 = graph.IDSWithButter(new_node.getState(), b.getNum(), p)
                     if tup2 is None:
@@ -58,17 +88,18 @@ class SolutionTreeIDS:
                     blocksTemp[new_node.getState().getRobot().getLocation()[0]][
                         new_node.getState().getRobot().getLocation()[1]].setHaveRobot(False)
 
-                    tmp=[PathNode(blocksTemp, new_node2.getState(), b.getNum, p.getNum,
-                                 currentNode.getPathString() + tup1[1] + tup2[1], 0, copy(unvisited_butters),
-                                 copy(unvisited_persons))]
+                    tmp = [PathNode(blocksTemp, new_node2.getState(), b.getNum(), p.getNum(),
+                                    currentNode.getPathString() + tup1[1] + tup2[1], 0, copy(unvisited_butters),
+                                    copy(unvisited_persons))]
 
-
-                    _thread.start_new_thread(self.startThread,(deepcopy(tmp),))
+                    # _thread.start_new_thread(self.startThread, (deepcopy(tmp),))
+                    temp.append(tmp)
                     # self.startThread(tmp)   # 5.20  min
 
-
-        # for q in finalList:
-        #     print(q.getPathString())
+        for q in temp:
+            _thread.start_new_thread(self.startThread, (deepcopy(q),))
+            for p in range(9900000):
+                pass
 
         if len(finalList) != 0:
             minPath = minLen = min(len(i.getPathString()) for i in finalList)
@@ -76,43 +107,36 @@ class SolutionTreeIDS:
                 if len(i.getPathString()) == minLen:
                     minPath = i.getPathString()
                     break
+            print(minPath)
+        #     f = open("output_files/outputs_IDS.txt", "w")
+        #     f.write(minPath + "\n" + str(minLen) + "\n" + str(minLen))
+        #     f.close()
+        # else:
+        #     f = open("output_files/outputs_IDS.txt", "w")
+        #     f.write("Impossible")
+        #     f.close()
+        #
+        # for i in finalList:
+        #     print("Path = {}".format(i.getPathString()))
 
-            f = open("output_files/outputs_IDS.txt", "w")
-            f.write(minPath + "\n" + str(minLen) + "\n" + str(minLen))
-            f.close()
-        else:
-            f = open("output_files/outputs_IDS.txt", "w")
-            f.write("Impossible")
-            f.close()
-
-        for i in finalList:
-            print("Path = {}".format(i.getPathString()))
-
-
-    def startThread(self,startingNodes1):
-        startingNodes=deepcopy(startingNodes1)
+    def startThread(self, startingNodes1):
+        startingNodes = deepcopy(startingNodes1)
         finalList = []
         while len(startingNodes) > 0:
-            currentNode = startingNodes.pop(0)
+            currentNode = startingNodes.pop()
             if len(currentNode.getUnvisitedButters()) == 0:
                 finalList.append(copy(currentNode))
                 continue
             for b in currentNode.getUnvisitedButters():
                 for p in currentNode.getUnvisitedPersons():
-                    for side in self.calculateEmptyAroundOfButter(b, currentNode.table):
-                        print(b.getNum(), " ", p.getNum(), " ", side)
-                        # print(b.getLocation())
-
-                        graph = GraphOperations(currentNode.table,deepcopy( self.__butters),
+                    for side in calculateEmptyAroundOfButter(b, currentNode.table):
+                        graph = GraphOperations(currentNode.table, deepcopy(self.__butters),
                                                 deepcopy(currentNode.getState().getRobot()), self.__persons)
-                        print("Start")
                         tup1 = graph.IDS(currentNode.getState(), b, side)
-                        print("end")
                         if tup1 is None:
                             continue
                         new_node = tup1[0]
-                        self.updateDataAfterSimpleIDS(new_node, currentNode.table, graph,currentNode)
-
+                        updateDataAfterSimpleIDS(new_node, currentNode.table, graph, currentNode)
 
                         tup2 = graph.IDSWithButter(new_node.getState(), b.getNum(), p)
                         if tup2 is None:
@@ -138,7 +162,7 @@ class SolutionTreeIDS:
                             new_node.getState().getRobot().getLocation()[1]].setHaveRobot(False)
 
                         startingNodes.append(
-                            PathNode(blocksTemp, new_node2.getState(), b.getNum, p.getNum,
+                            PathNode(blocksTemp, new_node2.getState(), b.getNum(), p.getNum(),
                                      currentNode.getPathString() + tup1[1] + tup2[1], 0, copy(unvisited_butters),
                                      copy(unvisited_persons)))
         # for q in finalList:
@@ -150,46 +174,14 @@ class SolutionTreeIDS:
                 if len(i.getPathString()) == minLen:
                     minPath = i.getPathString()
                     break
-
-            f = open("output_files/outputs_IDS.txt", "w")
-            f.write(minPath + "\n" + str(minLen) + "\n" + str(minLen))
-            f.close()
-        else:
-            f = open("output_files/outputs_IDS.txt", "w")
-            f.write("Impossible")
-            f.close()
-
-        for i in finalList:
-            print("Path = {}".format(i.getPathString()))
-
-
-    def updateDataAfterSimpleIDS(self, new_node, table, graph, parent):
-        table[parent.getState().getRobot().getLocation()[0]][parent.getState().getRobot().getLocation()[1]].setHaveRobot(False)
-        table[new_node.getState().getRobot().getLocation()[0]][
-            new_node.getState().getRobot().getLocation()[1]].setHaveRobot(True)
-        graph.blocks = table
-        graph.robot.setLocation((new_node.getState().getRobot().getLocation()[0],
-                                 new_node.getState().getRobot().getLocation()[1]))
-
-
-    def calculateEmptyAroundOfButter(self, butter, table):
-        ans = []
-        length = butter.getLocation()[1]
-        height = butter.getLocation()[0]
-        if table[height][length - 1].getHaveButter() or table[height][length - 1].getHaveObstacle():
-            pass
-        else:
-            ans.append(1)
-        if table[height - 1][length].getHaveButter() or table[height - 1][length].getHaveObstacle():
-            pass
-        else:
-            ans.append(2)
-        if table[height][length + 1].getHaveButter() or table[height][length + 1].getHaveObstacle():
-            pass
-        else:
-            ans.append(3)
-        if table[height + 1][length].getHaveButter() or table[height + 1][length].getHaveObstacle():
-            pass
-        else:
-            ans.append(4)
-        return ans
+            print(minPath)
+        #     f = open("output_files/outputs_IDS.txt", "w")
+        #     f.write(minPath + "\n" + str(minLen) + "\n" + str(minLen))
+        #     f.close()
+        # else:
+        #     f = open("output_files/outputs_IDS.txt", "w")
+        #     f.write("Impossible")
+        #     f.close()
+        #
+        # for i in finalList:
+        #     print("Path = {}".format(i.getPathString()))
