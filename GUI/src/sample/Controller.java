71a20841cc71ee5610a5559b19f637dba6bf2c85
Faraz <f.farangizadeh@gmail.com>
2021-04-26 11:19:59 +0430

    Debug GUI

--- GUI/src/sample/Controller.java
+++ GUI/src/sample/Controller.java
@@ -2,8 +2,6 @@ package sample;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.Scanner;
-
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,118 +12,219 @@ import javafx.scene.layout.GridPane;
 
 public class Controller implements Initializable
 {
-
     @FXML
-    private GridPane temp;
+    private GridPane gridPane;
 
     @FXML
     private JFXButton next;
 
+    @FXML
+    private JFXButton last;
+
+
     int[] HRobot = {3};
     int[] LRobot = {3};
     Table table = new Table();
-    String pathReal = " LRRDUDD";
+    String pathReal = "RRR";
+    int start = 0;
 
-    @FXML
-    void startDrawing(ActionEvent event)
+    public void UpdateTableNext(int row, int column, char m)
     {
-        int row = 5;
-        int column = 5;
-        String path = Character.toString(pathReal.charAt(0));
-        pathReal = pathReal.substring(1);
-
-        for (int p = 0; p < path.length(); p++)
+        if (m == 'L')
         {
-            temp.setVisible(false);
-            temp.setVisible(true);
-            char m = path.charAt(p);
-
-            if (m == 'L')
+            if (table.house[HRobot[0]][LRobot[0] - 1].butter)
             {
-                if (table.house[HRobot[0]][LRobot[0] - 1].butter)
-                {
-                    table.house[HRobot[0]][LRobot[0] - 1].butter = false;
-                    table.house[HRobot[0]][LRobot[0] - 2].butter = true;
-                }
-                table.house[HRobot[0]][LRobot[0]].robot = false;
-                table.house[HRobot[0]][LRobot[0] - 1].robot = true;
-                LRobot[0]--;
+                table.house[HRobot[0]][LRobot[0] - 1].butter = false;
+                table.house[HRobot[0]][LRobot[0] - 2].butter = true;
             }
+            table.house[HRobot[0]][LRobot[0]].robot = false;
+            table.house[HRobot[0]][LRobot[0] - 1].robot = true;
+            LRobot[0]--;
+        }
 
-            if (m == 'R')
+        if (m == 'R')
+        {
+            if (table.house[HRobot[0]][LRobot[0] + 1].butter)
             {
-                if (table.house[HRobot[0]][LRobot[0] + 1].butter)
-                {
-                    table.house[HRobot[0]][LRobot[0] + 1].butter = false;
-                    table.house[HRobot[0]][LRobot[0] + 2].butter = true;
-                }
-                table.house[HRobot[0]][LRobot[0]].robot = false;
-                table.house[HRobot[0]][LRobot[0] + 1].robot = true;
-                LRobot[0]++;
+                table.house[HRobot[0]][LRobot[0] + 1].butter = false;
+                table.house[HRobot[0]][LRobot[0] + 2].butter = true;
             }
+            table.house[HRobot[0]][LRobot[0]].robot = false;
+            table.house[HRobot[0]][LRobot[0] + 1].robot = true;
+            LRobot[0]++;
+        }
 
-            if (m == 'U')
+        if (m == 'U')
+        {
+            if (table.house[HRobot[0] - 1][LRobot[0]].butter)
             {
-                if (table.house[HRobot[0] - 1][LRobot[0]].butter)
-                {
-                    table.house[HRobot[0] - 1][LRobot[0]].butter = false;
-                    table.house[HRobot[0] - 2][LRobot[0]].butter = true;
-                }
-                table.house[HRobot[0]][LRobot[0]].robot = false;
-                table.house[HRobot[0] - 1][LRobot[0]].robot = true;
-                HRobot[0]--;
+                table.house[HRobot[0] - 1][LRobot[0]].butter = false;
+                table.house[HRobot[0] - 2][LRobot[0]].butter = true;
             }
+            table.house[HRobot[0]][LRobot[0]].robot = false;
+            table.house[HRobot[0] - 1][LRobot[0]].robot = true;
+            HRobot[0]--;
+        }
 
-            if (m == 'D')
+        if (m == 'D')
+        {
+            if (table.house[HRobot[0] + 1][LRobot[0]].butter)
             {
-                if (table.house[HRobot[0] + 1][LRobot[0]].butter)
-                {
-                    table.house[HRobot[0] + 1][LRobot[0]].butter = false;
-                    table.house[HRobot[0] + 2][LRobot[0]].butter = true;
-                }
-                table.house[HRobot[0]][LRobot[0]].robot = false;
-                table.house[HRobot[0] + 1][LRobot[0]].robot = true;
-                HRobot[0]++;
+                table.house[HRobot[0] + 1][LRobot[0]].butter = false;
+                table.house[HRobot[0] + 2][LRobot[0]].butter = true;
             }
+            table.house[HRobot[0]][LRobot[0]].robot = false;
+            table.house[HRobot[0] + 1][LRobot[0]].robot = true;
+            HRobot[0]++;
+        }
 
-            for (Node node : temp.getChildren())
+        updateGUI(row, column);
+    }
+
+    public void UpdateTableLast(int row, int column, char m)
+    {
+        if (m == 'L')
+        {
+            if (table.house[HRobot[0]][LRobot[0] + 1].butter)
             {
-                if (GridPane.getColumnIndex(node) != null)
-                {
-                    node.setVisible(false);
-                }
+                table.house[HRobot[0]][LRobot[0] + 1].butter = false;
+                table.house[HRobot[0]][LRobot[0]].butter = true;
             }
+            table.house[HRobot[0]][LRobot[0]].robot = false;
+            table.house[HRobot[0]][LRobot[0] - 1].robot = true;
+            LRobot[0]--;
+        }
 
-            for (int i = 1; i <= 5; i++)
+        if (m == 'R')
+        {
+            if (table.house[HRobot[0]][LRobot[0] - 1].butter)
             {
-                for (int j = 1; j <= 5; j++)
+                table.house[HRobot[0]][LRobot[0] - 1].butter = false;
+                table.house[HRobot[0]][LRobot[0]].butter = true;
+            }
+            table.house[HRobot[0]][LRobot[0]].robot = false;
+            table.house[HRobot[0]][LRobot[0] + 1].robot = true;
+            LRobot[0]++;
+        }
+
+        if (m == 'U')
+        {
+            if (table.house[HRobot[0] + 1][LRobot[0]].butter)
+            {
+                table.house[HRobot[0] + 1][LRobot[0]].butter = false;
+                table.house[HRobot[0]][LRobot[0]].butter = true;
+            }
+            table.house[HRobot[0]][LRobot[0]].robot = false;
+            table.house[HRobot[0] - 1][LRobot[0]].robot = true;
+            HRobot[0]--;
+        }
+
+        if (m == 'D')
+        {
+            if (table.house[HRobot[0] - 1][LRobot[0]].butter)
+            {
+                table.house[HRobot[0] - 1][LRobot[0]].butter = false;
+                table.house[HRobot[0]][LRobot[0]].butter = true;
+            }
+            table.house[HRobot[0]][LRobot[0]].robot = false;
+            table.house[HRobot[0] + 1][LRobot[0]].robot = true;
+            HRobot[0]++;
+        }
+        updateGUI(row, column);
+    }
+
+    public void updateGUI(int row, int column)
+    {
+        for (Node node : gridPane.getChildren())
+        {
+            if (GridPane.getColumnIndex(node) != null)
+            {
+                node.setVisible(false);
+            }
+        }
+
+        for (int i = 1; i <= row; i++)
+        {
+            for (int j = 1; j <= column; j++)
+            {
+                for (Node node : gridPane.getChildren())
                 {
-                    for (Node node : temp.getChildren())
+                    if (GridPane.getColumnIndex(node) != null)
                     {
-                        if (GridPane.getColumnIndex(node) != null)
+                        if (GridPane.getColumnIndex(node) == j && GridPane.getRowIndex(node) == i)
                         {
-                            if (GridPane.getColumnIndex(node) == j && GridPane.getRowIndex(node) == i)
+                            if (table.house[i][j].butter)
                             {
-                                if (table.house[i][j].butter)
-                                {
-                                    if (((ImageView) node).getImage().getUrl().endsWith("b.png"))
-                                        node.setVisible(true);
-                                }
-                                else if (table.house[i][j].robot)
-                                {
-                                    if (((ImageView) node).getImage().getUrl().endsWith("r.png"))
-                                        node.setVisible(true);
-                                }
+                                if (((ImageView) node).getImage().getUrl().endsWith("b.png"))
+                                    node.setVisible(true);
+                            }
+                            else if (table.house[i][j].robot)
+                            {
+                                if (((ImageView) node).getImage().getUrl().endsWith("r.png"))
+                                    node.setVisible(true);
                             }
                         }
                     }
                 }
             }
         }
+    }
 
-        for(int i = 1; i <= 5; i++)
+    @FXML
+    void drawNext(ActionEvent event)
+    {
+        int row = 5;
+        int column = 5;
+        char path = pathReal.charAt(start);
+        start++;
+
+        UpdateTableNext(row, column, path);
+        drawTableInConsole(row, column);
+
+        if(start == pathReal.length())
         {
-            for(int j = 1; j <= 5; j++)
+            next.setDisable(true);
+        }
+        if(start > 0)
+        {
+            last.setDisable(false);
+        }
+    }
+
+    @FXML
+    void drawLast(ActionEvent event)
+    {
+        int row = 5;
+        int column = 5;
+        start--;
+        char path = pathReal.charAt(start);
+        if(path == 'U')
+            path = 'D';
+        else if(path == 'D')
+            path = 'U';
+        else if(path == 'L')
+            path = 'R';
+        else if(path == 'R')
+            path = 'L';
+
+        UpdateTableLast(row, column, path);
+        drawTableInConsole(row, column);
+
+        if(start == 0)
+        {
+            last.setDisable(true);
+        }
+        if(start < pathReal.length())
+        {
+            next.setDisable(false);
+        }
+    }
+
+    public void drawTableInConsole(int row, int column)
+    {
+        for(int i = 1; i <= row; i++)
+        {
+            for(int j = 1; j <= column; j++)
             {
                 if(table.house[i][j].robot)
                 {
@@ -152,15 +251,11 @@ public class Controller implements Initializable
             System.out.println();
         }
         System.out.println("\n");
-        if(pathReal.length() == 0)
-        {
-            next.setDisable(true);
-        }
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
-
+        updateGUI(5, 5);
     }
 }
--- GUI/src/sample/Table.java
+++ GUI/src/sample/Table.java
@@ -7,30 +7,60 @@ public class Table
     public Table()
     {
         house = new House[5 + 2][5 + 2];
+        house[0][0] = new House(1, false, false, false, true);
+        house[0][1] = new House(1, false, false, false, true);
+        house[0][2] = new House(1, false, false, false, true);
+        house[0][3] = new House(1, false, false, false, true);
+        house[0][4] = new House(1, false, false, false, true);
+        house[0][5] = new House(1, false, false, false, true);
+        house[0][6] = new House(1, false, false, false, true);
+
+        house[1][0] = new House(1, false, false, false, true);
         house[1][1] = new House(1, false, false, false, false);
         house[1][2] = new House(1, false, false, false, false);
         house[1][3] = new House(1, false, false, false, false);
         house[1][4] = new House(1, false, false, false, false);
         house[1][5] = new House(1, false, false, false, false);
+        house[1][6] = new House(1, false, false, false, true);
+
+        house[2][0] = new House(1, false, false, false, true);
         house[2][1] = new House(1, false, false, false, false);
         house[2][2] = new House(1, false, false, false, false);
         house[2][3] = new House(1, false, false, false, false);
         house[2][4] = new House(1, false, false, false, false);
         house[2][5] = new House(1, false, false, false, false);
+        house[2][6] = new House(1, false, false, false, true);
+
+        house[3][0] = new House(1, false, false, false, true);
         house[3][1] = new House(1, false, false, false, false);
         house[3][2] = new House(1, false, false, false, false);
         house[3][3] = new House(1, true, false, false, false);
         house[3][4] = new House(1, false, true, false, false);
         house[3][5] = new House(1, false, false, false, false);
+        house[3][6] = new House(1, false, false, false, true);
+
+        house[4][0] = new House(1, false, false, false, true);
         house[4][1] = new House(1, false, false, false, false);
         house[4][2] = new House(1, false, false, false, false);
         house[4][3] = new House(1, false, false, false, false);
         house[4][4] = new House(1, false, false, false, false);
         house[4][5] = new House(1, false, false, false, false);
+        house[4][6] = new House(1, false, false, false, true);
+
+        house[5][0] = new House(1, false, false, false, true);
         house[5][1] = new House(1, false, false, false, false);
         house[5][2] = new House(1, false, false, false, false);
         house[5][3] = new House(1, false, false, false, false);
         house[5][4] = new House(1, false, false, false, false);
         house[5][5] = new House(1, false, false, false, false);
+        house[5][6] = new House(1, false, false, false, true);
+
+        house[6][0] = new House(1, false, false, false, true);
+        house[6][1] = new House(1, false, false, false, true);
+        house[6][2] = new House(1, false, false, false, true);
+        house[6][3] = new House(1, false, false, false, true);
+        house[6][4] = new House(1, false, false, false, true);
+        house[6][5] = new House(1, false, false, false, true);
+        house[6][6] = new House(1, false, false, false, true);
     }
 }
